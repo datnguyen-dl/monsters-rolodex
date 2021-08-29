9c281121ad238266dad841f92a5c00d29f33e02b
pvn-dat-nguyen <dat.nguyen@punch.vn>
2021-08-29 11:16:22 +0700

    Update more

--- src/App.js
+++ src/App.js
@@ -5,26 +5,43 @@ import './App.css';
 
 class App extends Component {
 
-  constructor() {
-    super()
-
-    this.state = {
-      monsters: [],
-      searchField: ''
-    }
-
-    // de bo code duoi ta dung Arrows Function: handleChange(e){} ----------> handleChange = (e) => {}
-    // this.handleChange = this.handleChange.bind(this)
+  // constructor(props) {
+  //   super(props) // gan props o tren va o day thi` moi' dung props trong Contructor duoc.
+  //   this.state = {
+  //     monsters: [],
+  //     searchField: '',
+  //     meaningOfLife: 47 + this.props.increment
+  //   }
+  //   // de bo code duoi ta dung Arrows Function: handleChange(e){} ----------> handleChange = (e) => {}
+  //   // this.handleChange = this.handleChange.bind(this)
+  // }
+  // ===> rut ngan nhu o duoi
+  // state = constructor(props) {super(props)}
+  state = {
+    monsters: [],
+    searchField: '',
+    meaningOfLife: 47
   }
-
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
   }
 
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value })
+  onSearchChange = (event) => {
+    this.setState({
+      searchField: event.target.value,
+    })
+  }
+
+  handleClick = () => {
+    // prevState = this.state
+    // prevProps = this.props
+    this.setState((prevState, prevProps) => {
+      return { meaningOfLife: prevState.meaningOfLife + prevProps.increment }
+    },
+      () => console.log(this.state.meaningOfLife)
+    )
   }
 
   render() {
@@ -35,11 +52,16 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h2>Asynchronous setState</h2>
+        <strong>{this.state.meaningOfLife}</strong><br />
+        <button onClick={this.handleClick}>State</button>
+        <h1>Monsters Rolodex List</h1><br />
         <SearchBox
           placeholder="Search monsters"
-          handleChange={this.handleChange}
+          handleChange={this.onSearchChange}
         />
         <CardList monsters={filteredMonsters} />
+
       </div>
     );
   }

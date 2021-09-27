5e067bebc01e1bddb53dcf7355567e6df6a713de
pvn-dat-nguyen <dat.nguyen@punch.vn>
2021-09-27 17:46:10 +0700

    addes redux, and implemented userReducer to header and app components

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,7 @@ import logger from 'redux-logger'
 import rootReducer from './root-reducer';
 
 const middlewares = [logger]
-// console.log('middlewares', middlewares)
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
-// console.log('store', store)
 
 export default store
